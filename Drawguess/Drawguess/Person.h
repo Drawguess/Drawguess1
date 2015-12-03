56b4c220eb77cf715931855e5c36d0e567b9398e
nickbeibeibei <nick_beibei@nick-beibeideMac-mini.local>
2015-12-03 16:31:08 +0800

    添加Person类

---
+++ Drawguess/Drawguess/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  Drawguess
+//
+//  Created by nick_beibei on 15/12/3.
+//  Copyright © 2015年 nick_beibei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ Drawguess/Drawguess/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  Drawguess
+//
+//  Created by nick_beibei on 15/12/3.
+//  Copyright © 2015年 nick_beibei. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
